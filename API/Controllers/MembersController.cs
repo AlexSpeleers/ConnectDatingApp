1988c009f40296c6c44c72fad07488ea8470724a
BRISINGR <alexsieger@gmail.com>
2026-01-07 04:01:11 +0100

    Create basic web api + empty client project

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MembersController(AppDbContext context) : Controller
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        List<AppUser> members = await context.Users.ToListAsync();
+        return Ok(members);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        AppUser? member = await context.Users.FindAsync(id);
+        if (member == null) return NotFound();
+        return Ok(member);
+    }
+}
